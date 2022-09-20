64f6ab169a58531217e2498883af4b3b23f212fa
chezmarcbrown <marc@chezbrown.org>
2022-09-20 13:08:51 -0800

    fleshed out more pieces... still todo: util fns

---
+++ baseball/gamedata/clean-gamelog.py
@@ -0,0 +1,31 @@
+import csv
+
+# https://www.retrosheet.org/boxesetc/1967/Y_1967.htm
+
+INFIELDS = ('Date', 'NumberOfGame', 'DayOfWeek',
+          'Visiting Team', 'Visiting Team League', 'Visiting Team Game #',
+          'Home Team', 'Home Team League', 'Home Team Game #',
+          'Visitors Score', 'Home Score')
+
+OUTFIELDS = ('Date', 'Visiting Team League', 'Visiting Team', 'Visitors Score', 'Home Team', 'Home Score')
+
+def process_file(filename):
+    print(f"***FILE: {filename}")
+    N = 0
+    with open(filename, newline='') as incsvfile:
+        with open('games.csv', 'w', newline='') as outcsvfile:
+            writer = csv.DictWriter(outcsvfile, fieldnames=OUTFIELDS,  extrasaction='ignore')
+            writer.writeheader()
+            reader = csv.DictReader(incsvfile, INFIELDS)
+            for row in reader:
+                if row['Visiting Team League'] in ['AL', 'NL']:
+                    h = int(row['Home Score'])
+                    v = int(row['Visitors Score'])
+                    if h != v:
+                        N += 1
+                        writer.writerow(row)
+    print(f'Lines written: {N}')
+
+
+if __name__ == '__main__':
+    process_file("GL1967.TXT")
---
+++ baseball/gamedata/util.py
@@ -0,0 +1,27 @@
+# unsorted list of tuples: date, league, visitor, vscore, home, hscore
+gamelog = None 
+
+# maps team code to team name, e.g., "BOS" to "Boston Red Sox"
+teamcodes = {}
+
+def standings(date, league='AL'):
+    """ Returns standings for specified league as of a specified date. """
+    if not gamelog: 
+        initialize_gamelog()
+        initialize_teamcodes()
+    
+    # unsorted list of tuples: teamcode, wins, losses
+    standings = []
+    for g in gamelog:
+        pass
+    return standings        
+    
+def teamname(teamcode):
+    return None
+
+def initialize_gamelog():
+    pass
+
+def initialize_teamcodes():
+    pass
+    
