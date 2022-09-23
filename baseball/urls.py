4c9e9493298bcf922cfe65926006c315ac5d7995
chezmarcbrown <marc@chezbrown.org>
2022-09-22 20:48:16 -0800

    fixed logo; added urls for baseball/redsox and baseball/oneteam/xxx

--- baseball/urls.py
+++ baseball/urls.py
@@ -5,4 +5,7 @@ app_name = "baseball"
 urlpatterns = [
     path("", views.index, name="index"),
     path("standings", views.standings, name="standings"),
+    path('logos', views.logos, name="logos"),    
+    path('redsox', views.redsox, name="redsox"),
+    path('oneteam/<str:code>', views.oneteam, name="oneteam"),
 ]
--- baseball/views.py
+++ baseball/views.py
@@ -3,8 +3,11 @@ from datetime import datetime
 from . import util
 
 # Create your views here.
+
+
 def index(request):
     return render(request, "baseball/index.html")
+
 
 def standings(request):
     date_str = request.GET["date"]
@@ -13,3 +16,29 @@ def standings(request):
     standings = util.standings(d, league='AL')
     context = {"standings": standings, "date": d}
     return render(request, "baseball/standings.html", context)
+
+
+def logos(request):
+    d = datetime.strptime("2022-01-01", '%Y-%m-%d').date()
+    standings = []
+    for l in ['AL', 'NL']:
+        standings.extend(util.standings(d, league=l))
+    logos = []
+    for s in standings:
+        code = s.code
+        name = util.team_name(code)
+        url = util.team_logo(code)
+        logos.append((code, name, url))
+    return render(request, "baseball/logos.html", {"logos": logos})
+
+
+def redsox(request):
+    return oneteam("BOS")
+
+
+def oneteam(request, code):
+    code = code.upper()
+    name = util.team_name(code)
+    url = util.team_logo(code)
+    return render(request, "baseball/oneteam.html",
+                  {"code": code, "name": name, "url": url})
