6d903948c6e2a48859e9db10fb4c7561cd6482e9
Nicole Mah <mnmah@alaska.edu>
2022-09-26 16:42:59 -0800

    Change Index html to add radio buttons

--- baseball/views.py
+++ baseball/views.py
@@ -11,10 +11,25 @@ def index(request):
 
 def standings(request):
     date_str = request.GET["date"]
+    league = request.GET['league']
     # assumes date is valid
     d = datetime.strptime(date_str, '%Y-%m-%d').date()
     standings = util.standings(d, league='AL')
-    context = {"standings": standings, "date": d}
+    end_date = "1967-10-1"
+    end_season = datetime.strptime(end_date,'%Y-%m-%d').date()
+
+    if d >= end_season:
+        final = True
+    else:
+        final = False
+
+    logos = []
+    for s in standings:
+        code = s.code
+        name = util.team_name(code)
+        url = util.team_logo(code)
+        logos.append((code, name, url))
+    context = {"standings": standings, "date": d, "logos": logos, "end_season": final, "league": league}
     return render(request, "baseball/standings.html", context)
 
 
